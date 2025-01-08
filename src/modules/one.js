86d37e5f778d34bbdc88c4e0568190e24bf20de3
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-08 14:01:16 +0600

    Lesson18

---
+++ src/modules/one.js
@@ -0,0 +1,6 @@
+const one = () => {
+  console.log("Вызов первого файла js");
+  console.log("Вызов первого файла js");
+};
+
+export default one;
---
+++ src/modules/two.js
@@ -0,0 +1,5 @@
+const two = () => {
+  console.log("Вызов второго файла js");
+};
+
+module.exports = two;
