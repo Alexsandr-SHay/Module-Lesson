6cb6e870f0083b996084df9964d24ed109863831
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-12 23:26:14 +0600

    Урок21

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import dataEntryForm from "./modules/dataEntryForm";
 
 timer("27 january 2025");
 menu();
 modal();
+dataEntryForm();
