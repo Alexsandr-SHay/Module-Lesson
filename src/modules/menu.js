3bc9e955619b8711066c85bcdf4bf154ac1db3df
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-15 02:23:02 +0600

    Испавленное 22 задание

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -8,38 +8,35 @@ const menu = () => {
     menu.classList.toggle("active-menu");
   };
 
-  menuBtn.addEventListener("click", handleMenu);
+  //ПРостое задание.
+  // menuBtn.addEventListener("click", handleMenu);
 
-  // closeBtn.addEventListener("click", handleMenu);
-
-  // menuItems.forEach((menuItem) =>
-  //   menuItem.addEventListener("click", handleMenu)
-  // );
-
-  menu.addEventListener("click", (e) => {
-    if (e.target.closest("ul>li>a") || e.target.closest(".close-btn")) {
-      handleMenu();
-    }
-  });
-
-  // document.addEventListener("click", (e) => {
-  //   if (e.target.closest(".menu")) {
-  //     handleMenu();
-  //   }
-
-  //   if (e.target.matches("menu>ul>li>a") || e.target.closest(".close-btn")) {
-  //     handleMenu();
-  //   }
-
-  //   if (
-  //     !e.target.closest("menu") &&
-  //     !e.target.closest(".menu") &&
-  //     menu.classList.contains("active-menu")
-  //   ) {
-  //     console.log("Я молодец");
+  //   menu.addEventListener("click", (e) => {
+  //   if (e.target.closest("ul>li>a") || e.target.closest(".close-btn")) {
   //     handleMenu();
   //   }
   // });
+
+  //Сложное задание
+  document.addEventListener("click", (e) => {
+    console.log(!e.target.closest("menu"));
+
+    if (e.target.closest(".menu")) {
+      handleMenu();
+    }
+
+    if (e.target.matches("menu>ul>li>a") || e.target.closest(".close-btn")) {
+      handleMenu();
+    }
+
+    if (
+      !e.target.closest("menu") &&
+      !e.target.closest(".menu") &&
+      menu.classList.contains("active-menu")
+    ) {
+      handleMenu();
+    }
+  });
 };
 
 export default menu;
