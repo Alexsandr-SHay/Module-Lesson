ffc180205dac615451ee7313459be5838f0d8a8f
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-16 02:54:03 +0600

    Урок 23 слайдер карусель

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import dataEntryForm from "./modules/dataEntryForm";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import sliderCarousel from "./modules/sliderCarousel";
 
 timer("27 january 2025");
 menu();
@@ -11,3 +12,4 @@ modal();
 dataEntryForm();
 tabs();
 slider();
+sliderCarousel();
