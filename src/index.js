cf0056d34933c2a0092905cf1fba58b405e7cd76
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-16 01:13:55 +0600

    Урок23

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import dataEntryForm from "./modules/dataEntryForm";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("27 january 2025");
 menu();
 modal();
 dataEntryForm();
 tabs();
+slider();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,26 +1,11 @@
 const menu = () => {
-  const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
-  const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a");
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
   };
 
-  //ПРостое задание.
-  // menuBtn.addEventListener("click", handleMenu);
-
-  //   menu.addEventListener("click", (e) => {
-  //   if (e.target.closest("ul>li>a") || e.target.closest(".close-btn")) {
-  //     handleMenu();
-  //   }
-  // });
-
-  //Сложное задание
   document.addEventListener("click", (e) => {
-    console.log(!e.target.closest("menu"));
-
     if (e.target.closest(".menu")) {
       handleMenu();
     }
