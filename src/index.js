2519bf92b392b794b82882543be0b1e033a3d1e9
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-23 23:07:21 +0600

    27.1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import sliderCarousel from "./modules/sliderCarousel";
 import calc from "./modules/calc";
+import sendForm from "./modules/sendForm";
 
 timer("27 january 2025");
 menu();
@@ -22,3 +23,32 @@ slider(
 );
 sliderCarousel();
 calc(100);
+sendForm({
+  formId: "form1",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+
+sendForm({
+  formId: "form2",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+
+sendForm({
+  formId: "form3",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
